6e428debd43e20e07cc3c1b8d9bf568f9a24dbe0
ozgedrk <121420646+ozgedrk@users.noreply.github.com>
2024-05-24 21:29:30 +0300

    Edit and Create Screens

---
+++ components/BlogPostForm.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+
+export default function BlogPostForm() {
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('')
+
+  return (
+    <View style={styles.Main} >
+      <Text style={styles.Label} >Enter The Title:</Text>
+      <TextInput style={styles.Input}
+        value={title}
+        onChangeText={(text)=>setTitle(text)} />
+      <Text style={styles.Label} >Enter The Content:</Text>
+      <TextInput style={styles.Input}
+       value={content}
+       onChangeText={(text)=>setContent(text)} />
+      <TouchableOpacity style={styles.ButtonMain}>
+        <View style={styles.ButtonView}>
+          <Text style={styles.ButtonText}>Save</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    Main:{
+    marginTop: 10,
+    },
+    Label:{
+    fontSize: 20,
+    marginLeft: 10,
+    },
+    Input:{
+    borderWidth:1,
+    margin: 10,
+    borderRadius: 20,
+    padding: 5,
+    fontSize: 18,
+    marginBottom: 15,
+    },
+    ButtonMain:{
+    padding: 30,
+    },
+    ButtonView:{
+    backgroundColor: 'green',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent:  'center',
+    borderRadius: 20,
+    },
+    ButtonText:{
+    color: 'white', 
+    fontSize: 20,
+    },
+})
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -3,23 +3,25 @@ import React, { useContext } from 'react';
 import {Context} from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-export default function IndexScreen() {
+export default function IndexScreen( {navigation} ) {
   const {state , addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
-      {/* <Text>IndexScreen </Text> */}
       <Button title="Add" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.id}
         renderItem={({ item }) => {
           return (
-          <View style={styles.Row} > 
-            <Text style={styles.Title} >{item.title}</Text>
-            <TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
-            <Feather name="trash" size={24} color="black" />
-            </TouchableOpacity>
-          </View>);
+            <TouchableOpacity onPress={()=> navigation.navigate('Show',{id:item.id})} >
+              <View style={styles.Row} > 
+                <Text style={styles.Title} >{item.title}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
+                <Feather name="trash" size={24} color="black" />
+                </TouchableOpacity>
+              </View>
+          </TouchableOpacity>
+          );
         }}
       />
     </View>
